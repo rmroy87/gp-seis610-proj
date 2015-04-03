abf39219c34b63bcb0045c9a706fe1a822101939
hthrsmms <SIMM5792@STTHOMAS.EDU>
2015-04-03 13:02:00 -0500

    h

--- Source/Lucy/src/engine/CrossoverOperator.java
+++ Source/Lucy/src/engine/CrossoverOperator.java
@@ -1,23 +1,5 @@
 package engine;
 
-import java.util;
-
 public class CrossoverOperator {
-	
-	
-	private static Individual crossover(Individual indiv1, Individual indiv2) {
-        Individual newSol = new Individual();
-        // Loop through nodes 
-        for (int i = 0; i < indiv1.size(); i++) {
-            // Crossover
-            if (java.util.Random() <= Individual.size()) {
-                newSol.setNode(i, indiv1.getNode(i));
-            } else {
-                newSol.setNode(i, indiv2.getNode(i));
-            }
-        }
-        return newSol;
-	}
-	
 
 }
--- Source/Lucy/src/engine/MutationOperator.java
+++ Source/Lucy/src/engine/MutationOperator.java
@@ -1,17 +1,5 @@
 package engine;
 
 public class MutationOperator {
-	
-    // Mutate an individual
-    private static void mutate(Individual indiv) {
-        // Loop through nodes
-        for (int i = 0; i < indiv.size(); i++) {
-            if (java.util.Random() <= mutationRate) {
-                // Create random node
-                byte gene = (byte) java.util.Random());
-				indiv.setNode(i, node);
-            }
-        }
-    }
 
 }
