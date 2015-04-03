985ef3ea9675ae3c4f03add830d73ea5b5358ce3
hthrsmms <SIMM5792@STTHOMAS.EDU>
2015-04-03 13:14:55 -0500

    updating classes
    population, mutation, crossover

--- Source/Lucy/src/engine/CrossoverOperator.java
+++ Source/Lucy/src/engine/CrossoverOperator.java
@@ -1,5 +1,21 @@
 package engine;
 
 public class CrossoverOperator {
+	private static Individual crossover(Individual indiv1, Individual indiv2) {
+        Individual newSol = new Individual();
+        // Loop through nodes 
+        for (int i = 0; i < indiv1.size(); i++) {
+            // Crossover
+            if (java.util.Random() <= Individual.size()) {
+                newSol.setNode(i, indiv1.getNode(i));
+            } else {
+                newSol.setNode(i, indiv2.getNode(i));
+            }
+        }
+        return newSol;
+	}
+	
 
 }
+
+
