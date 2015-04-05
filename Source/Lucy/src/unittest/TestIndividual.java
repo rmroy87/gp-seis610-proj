e834c696c7f898ff53672e1b868a9aa5fd820c90
rmroy87 <robertr@visi.com>
2015-04-04 23:22:21 -0500

    Support testing fitness calculation for all individuals.

--- Source/Lucy/src/unittest/TestIndividual.java
+++ Source/Lucy/src/unittest/TestIndividual.java
@@ -10,7 +10,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestIndividual {
-	Individual[] population = new Individual[200];
+	Individual[] population = new Individual[500];
+	float calcFitness;
 	@Test
 	public void test() throws SecurityException, FileNotFoundException, IOException {
 		LogManager.getLogManager().readConfiguration(new FileInputStream("./logging.properties"));
@@ -19,14 +20,16 @@ public class TestIndividual {
 		
 		System.out.println("Individual Unit Test: (The Population)\n");
 		
-		//for(int i = 0; i < settings.InitPopulationSize; i = i+1){
-		for(int i = 0; i < 10; i = i+1){
-			
+		for(int i = 0; i < settings.InitPopulationSize; i = i+1){
+		//for(int i = 0; i < 10; i = i+1){
+					
 			population[i] = new Individual();
 			System.out.print(String.format("Individual[%d]--> f(x) = ", i));			
 			System.out.print(population[i].ToString());
-			System.out.print(String.format("Individual[%d]--> Fitness = %4.2f ", 
-					population[i].CalculateFitness() ));
+			System.out.println();
+			calcFitness = population[i].CalculateFitness();
+			System.out.print(String.format("Individual[%d]--> Fitness = %4.2f ",i, calcFitness ));
+			System.out.println();
 			System.out.println();
 		}		
 	}
