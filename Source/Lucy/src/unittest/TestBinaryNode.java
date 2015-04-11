05ec48a04fe72ab74f478db0abbe2244c3f65e4b
rmroy87 <robertr@visi.com>
2015-04-10 23:27:07 -0500

    Cleaned up and added tests to demonstrate the Mutation, Cross-over, random selection and random insertion.

--- Source/Lucy/src/unittest/TestBinaryNode.java
+++ Source/Lucy/src/unittest/TestBinaryNode.java
@@ -13,16 +13,28 @@ import org.junit.Test;
 public class TestBinaryNode {
 	//ArrayList<BinaryNode> nodeList = new ArrayList<BinaryNode>();  
 	
+	
 	@Test
 	public void test() throws SecurityException, FileNotFoundException, IOException {
 		LogManager.getLogManager().readConfiguration(new FileInputStream("./logging.properties"));
 		
+		test_creation_string_fitness();
+		
+		test_deepcopy_random_select();
+		
+		test_random_select_insert();
+		
+	}
+	
+	public void test_creation_string_fitness()
+	{
 		float nodeVal = 0;
 		Settings settings = Settings.get();
 		BinaryNode[] binNodes = new BinaryNode[10];
+				
 		int[] x_domain = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
 		
-		System.out.println("Binary Node Unit Test:\n");
+		System.out.println("Binary Node Unit Test:");
 		
 		for(int i = 0; i < settings.MaxTreeDepth; i = i+1) {
 			binNodes[i] = new BinaryNode(null, i);			
@@ -38,7 +50,80 @@ public class TestBinaryNode {
 			}
 			System.out.println();
 			System.out.println();
+		}		
+	}
+	
+	public void test_deepcopy_random_select(){
+		
+		BinaryNode testCopy = null;
+		
+		BinaryNode OrigNode = null;
+		BinaryNode RandSelectNode = null;
+		
+		System.out.println();
+		System.out.println("Test the Deep Copy and Random Select:");
+		OrigNode = new BinaryNode(null, 3);
+		System.out.print(String.format("Original->Equation --> f(x) = %s\n", 
+				OrigNode.ResolveNodeString()));
+		testCopy = OrigNode.DeepCopy();
+		System.out.print(String.format("Copy->Equation     --> f(x) = %s\n\n", 
+				testCopy.ResolveNodeString()));
+		
+		for(int j=0; j < 10; j=j+1){
+			RandSelectNode = OrigNode.GetBinaryNodeRandomly();
+			System.out.print(String.format("Random Select[%d]->Equation   --> f(x) = %s\n", 
+					j, RandSelectNode.ResolveNodeString()));
 		}
+		
+	}
+	
+	
+	public void test_random_select_insert(){
+		BinaryNode TestParent1 = null;
+		BinaryNode TestParent2 = null;
+		BinaryNode OffSpring = null;
+		BinaryNode CrossedOverNode = null;
+		
+		
+		TestParent1 = new BinaryNode(null, 3);
+		TestParent2 = new BinaryNode(null, 4);
+		
+		OffSpring       = TestParent1.DeepCopy();
+		CrossedOverNode = TestParent2.GetBinaryNodeRandomly();
+		
+		System.out.println("\nTest the Random Copy and Random Insert:");
+		
+		System.out.print(String.format("Parent 1 -->        Equation   --> f(x) = %s Depth Top[%d]:L[%d]:R[%d]\n", 
+				TestParent1.ResolveNodeString(),
+				TestParent1.GetNodeDepth(),
+				TestParent1.GetLeftNodeDepth(),
+				TestParent1.GetRightNodeDepth() ));		
+	
+		System.out.print(String.format("Parent 2 -->        Equation   --> f(x) = %s Depth Top[%d]:L[%d]:R[%d]\n", 
+				TestParent2.ResolveNodeString(),
+				TestParent2.GetNodeDepth(),
+				TestParent2.GetLeftNodeDepth(),
+				TestParent2.GetRightNodeDepth() ));		
+	
+		System.out.print(String.format("Offspring Start --> Equation   --> f(x) = %s Depth Top[%d]:L[%d]:R[%d]\n", 
+				OffSpring.ResolveNodeString(),
+				OffSpring.GetNodeDepth(),
+				OffSpring.GetLeftNodeDepth(),
+				OffSpring.GetRightNodeDepth() ));		
+		
+		System.out.print(String.format("Selected Node -->   Equation   --> f(x) = %s Depth Top[%d]:L[%d]:R[%d]\n", 
+				CrossedOverNode.ResolveNodeString(),
+				CrossedOverNode.GetNodeDepth(),
+				CrossedOverNode.GetLeftNodeDepth(),
+				CrossedOverNode.GetRightNodeDepth() ));
+		
+		OffSpring.InsertBinaryNodeRandomly(CrossedOverNode);
+		
+		System.out.print(String.format("Offspring Cross --> Equation   --> f(x) = %s Depth Top[%d]:L[%d]:R[%d]\n", 
+				OffSpring.ResolveNodeString(),
+				OffSpring.GetNodeDepth(),
+				OffSpring.GetLeftNodeDepth(),
+				OffSpring.GetRightNodeDepth() ));	
 	}
 
 }
--- Source/Lucy/src/unittest/TestIndividual.java
+++ Source/Lucy/src/unittest/TestIndividual.java
@@ -11,18 +11,100 @@ import org.junit.Test;
 
 public class TestIndividual {
 	Individual[] population = new Individual[500];
+	float bestFitness = (float)1000.0;
+	int bestIndex = 0;
+	int bestJ = 0;
+	String bestEquation;
 	float calcFitness;
+	
 	@Test
 	public void test() throws SecurityException, FileNotFoundException, IOException {
 		LogManager.getLogManager().readConfiguration(new FileInputStream("./logging.properties"));
 		
-		Settings settings = Settings.get();
+		//Settings settings = Settings.get();
 		
-		System.out.println("Individual Unit Test: (The Population)\n");
+		TestMutation();
 		
-		for(int i = 0; i < settings.InitPopulationSize; i = i+1){
-		//for(int i = 0; i < 10; i = i+1){
-					
+		//TestCreateEntirePopulation(500);
+		
+		TestCrossOver();
+		
+		//TestPopulationInLoopUntilZeroFitness(500, 10000);		
+		
+	}
+	
+	/*
+	 * Unit test to test the basic Mutation function.
+	 */
+	public void TestMutation(){
+	
+		Individual parent1 = new Individual();
+		Individual parent2 = new Individual();
+		Individual mutatedIndividual1 = parent1.DeepCopyClone();
+		Individual mutatedIndividual2 = parent2.DeepCopyClone();
+		
+		System.out.println(String.format("\nIndividual - Unit Test: (Mutate Individuals)\n"));
+		
+		System.out.print(String.format("Individual 1 ------> f(x) = %s [Fitness = %f]\n", parent1.ToString(), parent1.getFitnessValue() ));
+		System.out.print(String.format("Individual 2 ------> f(x) = %s [Fitness = %f]\n", parent2.ToString(), parent2.getFitnessValue() ));
+		System.out.print(String.format("Cloned 1 ----------> f(x) = %s [Fitness = %f]\n", mutatedIndividual1.ToString(), mutatedIndividual1.getFitnessValue() ));
+		System.out.print(String.format("Cloned 2 ----------> f(x) = %s [Fitness = %f]\n\n", mutatedIndividual2.ToString(), mutatedIndividual2.getFitnessValue() ));
+		
+		mutatedIndividual1.ModifyIndividualRandomly();
+		mutatedIndividual2.ModifyIndividualRandomly();
+		
+				
+		System.out.print(String.format("Mutated Clone 1 ---> f(x) = %s [Fitness = %f]\n", 
+				mutatedIndividual1.ToString(), mutatedIndividual1.getFitnessValue() ));
+		System.out.print(String.format("Mutated Clone 2 ---> f(x) = %s [Fitness = %f]\n\n",
+				mutatedIndividual2.ToString(), mutatedIndividual2.getFitnessValue() ));
+				
+	}
+	
+	/*
+	 *  Unit Test to test the Cross-Over function
+	 */
+	public void TestCrossOver(){
+		BinaryNode crossSelect1 = null;
+		BinaryNode crossSelect2 = null;
+		Individual parent1 = new Individual();
+		Individual parent2 = new Individual();
+		Individual offspring1 = parent1.DeepCopyClone();
+		Individual offspring2 = parent2.DeepCopyClone();
+		
+		System.out.println(String.format("\nIndividual - Unit Test: (Cross-Over Individuals)\n"));
+		
+		System.out.print(String.format("Parent 1 -----> f(x) = %s [Fitness = %f]\n", parent1.ToString(), parent1.getFitnessValue() ));
+		System.out.print(String.format("Parent 2 -----> f(x) = %s [Fitness = %f]\n", parent2.ToString(), parent2.getFitnessValue() ));
+		System.out.print(String.format("Cloned 1 -----> f(x) = %s [Fitness = %f]\n", offspring1.ToString(), offspring1.getFitnessValue() ));
+		System.out.print(String.format("Cloned 2 -----> f(x) = %s [Fitness = %f]\n\n", offspring2.ToString(), offspring2.getFitnessValue() ));
+		
+		crossSelect1 = parent1.GetBinaryNodeRandomly();
+		crossSelect2 = parent2.GetBinaryNodeRandomly();
+		
+		System.out.print(String.format("Cross Select 1 --> f(x) = %s\n", crossSelect1.ResolveNodeString()) );
+		System.out.print(String.format("Cross Select 2 --> f(x) = %s\n\n", crossSelect2.ResolveNodeString() ));
+		
+		offspring1.InsertBinaryNodeRandomly(crossSelect2);
+		offspring2.InsertBinaryNodeRandomly(crossSelect1);
+		
+		System.out.print(String.format("Offspring 1 --> f(x) = %s [Fitness = %f]\n", offspring1.ToString(), offspring1.getFitnessValue() ));
+		System.out.print(String.format("Offspring 2 --> f(x) = %s [Fitness = %f]\n\n", offspring2.ToString(), offspring2.getFitnessValue() ));
+	
+	}
+	/*
+	 * Test to create an entire Population of the size passed in, max of 500
+	 */
+	public void TestCreateEntirePopulation( int populationSize){
+			
+		if( populationSize > 500){
+			populationSize = 500;
+		}
+		
+		System.out.println(String.format("\nIndividual - Unit Test: (Create The Population) - Size = %d\n", populationSize));
+		
+		
+		for(int i = 0; i < populationSize; i = i+1){					
 			population[i] = new Individual();
 			System.out.print(String.format("Individual[%d]--> f(x) = ", i));			
 			System.out.print(population[i].ToString());
@@ -31,6 +113,70 @@ public class TestIndividual {
 			System.out.print(String.format("Individual[%d]--> Fitness = %4.2f ",i, calcFitness ));
 			System.out.println();
 			System.out.println();
-		}		
+			
+			if(calcFitness < bestFitness){
+				bestFitness = calcFitness;
+				bestIndex = i;			
+				bestEquation = population[i].ToString();
+			}		 
+		}
+		System.out.println();
+		System.out.print(String.format("\nBest Fitness Found = %4.2f, Index = %d ", bestFitness, bestIndex ));
+		System.out.println();		
 	}
+	
+	/*
+	 * Create Populations over and over until a zero Fitness is found
+	 */
+	public void TestPopulationInLoopUntilZeroFitness(int populationSize, int maxLoopCount){
+		
+		if( populationSize > 500){
+			populationSize = 500;
+		}
+		System.out.println(String.format("Individual - Unit Test: (Loop Until Fitness == ZERO) - Size = %d, Loop = %d\n", 
+				populationSize, maxLoopCount));
+		
+		for(int j=0; j < maxLoopCount; j = j+1){
+			
+			for(int i = 0; i < populationSize; i = i+1){			
+						
+				population[i] = new Individual();
+				//System.out.print(String.format("Individual[%d]--> f(x) = ", i));			
+				//System.out.print(population[i].ToString());
+				//System.out.println();
+				calcFitness = population[i].getFitnessValue();
+				//System.out.print(String.format("Individual[%d]--> Fitness = %4.2f ",i, calcFitness ));
+				//System.out.println();
+				//System.out.println();
+				
+				if(calcFitness < bestFitness){
+					bestFitness = calcFitness;
+					bestIndex = i;
+					bestJ = j;
+					bestEquation = population[i].ToString();
+					if(bestFitness == 0){
+						System.out.println("FITNESS == ZERO found!!");
+						break;
+					}
+				}			 
+			}
+			
+			System.out.print(String.format("Loop - Best Fitness Found = %f, Index = %d, Loop = %d\n ", bestFitness, bestIndex, bestJ ));
+			
+			
+			for(int i = 0; i < populationSize; i = i+1){
+				population[i] = null;
+			}
+			
+			if(bestFitness == 0.00){
+				break;
+			}
+		}
+		
+		System.out.println();
+		System.out.print(String.format("\nComplete Test - Best Fitness Found = %f, Index = %d, Loop = %d - String = %s", 
+				bestFitness, bestIndex, bestJ, bestEquation ));
+		System.out.println();
+	}
+	
 }
