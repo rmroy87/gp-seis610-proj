2d6d6ef7aab07b06f1472af96ef1449ebf47643a
rmroy87 <robertr@visi.com>
2015-04-23 14:10:42 -0500

    Cleaned up a few things with array lists.

--- Source/Lucy/src/engine/Population.java
+++ Source/Lucy/src/engine/Population.java
@@ -20,115 +20,158 @@ public class Population {
 	  private Random rand; // random generator
 
     
+	  /*
+	   * This constructor is used to create the INITIAL POPULATION, once an 
+	   * initial population has been created, use the other constructor to
+	   * work with the succeeding generations of a population.
+	   */
 	  public Population()
 	  { 
-		Settings settings = Settings.get();
-	    
-		//creates initial population
-		//individuals = new Individual[settings.InitPopulationSize];
-		
-			for(int i = 0; i < settings.InitPopulationSize; i++){					
-				//individuals[i] = new Individual();
+			Settings settings = Settings.get();
+						
+			for(int i = 0; i < settings.InitPopulationSize; i++){			
 				individuals.add(new Individual());
 			}
-
-		/*
-		* calculate the number of individuals to crossover
-		*/
-	    CrossoverSize = (int)(settings.CrossoverProbability*settings.PopulationSize); 
-	     // needs to be a multiple of 2 
-	      if (CrossoverSize % 2 != 0)
-		CrossoverSize++;
+			
+			/*
+			* calculate the number of individuals to crossover
+			*/
+			CrossoverSize = (int)(settings.CrossoverProbability*settings.PopulationSize); 
+			 // needs to be a multiple of 2 
+			if (CrossoverSize % 2 != 0)
+				  CrossoverSize++;
+			
+			/*
+			* calculate the number of individuals to mutate
+			*/
+			MutateSize = (int)(settings.MutationProbability*settings.PopulationSize);
+			
+			/*
+			 * Sort he Population Array List, so that they are ordered based on
+			 * the best fitness value
+			 */
 	    
-	    /*
-		* calculate the number of individuals to mutate
-		*/
-	    MutateSize = (int)(settings.MutationProbability*settings.PopulationSize);
-	  }
+	   }
 	  
+		  
+	  /*
+	   * This constructor is used to work on the "Next Generation" of a population.
+	   */  
 	  public Population(Population genX)
 	  { 
-		Settings settings = Settings.get();
-	    // Stub in
+			Settings settings = Settings.get();
+			
+			
+			
+		    // Stub in
 	  }
 
-	    /*
-		* sort individuals by highest fitness value
-		*/
+	/*
+	* sort individuals by highest fitness value
+	*/
 	  private void selectBest(int n) 
 	  {
-	    populationSize = n ;
-	    population = new int[populationSize];
-
-	    double[] fitnessValues = new double[N];
-	    for (int i = 0; i < N; i++)
-	      fitnessValues[i] = individuals.get(i).getFitnessValue();
-	  
-	    FitnessSelectionOperator.selectBest(fitnessValues, N,  
-					population, populationSize);
+		    populationSize = n ;
+		    population = new int[populationSize];
+	
+		    double[] fitnessValues = new double[N];
+		    
+		    for (int i = 0; i < N; i++)
+		      fitnessValues[i] = individuals.get(i).getFitnessValue();
+		
+		  
+		    FitnessSelectionOperator.selectBest(fitnessValues, N,  
+						population, populationSize);
 	  }
 
-	    /*
-		* probability of being selected 
-		*/
+	/*
+	* probability of being selected 
+	*/
 	  private void selectProb(int n)
 	  {
-    // compute individual selection probabilities based on fitness
-	    computeProbabilities();
-	    
-	    populationSize = n;
-	    population = new int[populationSize];
-	    FitnessSelectionOperator.selectProb(N, probabilities, population, populationSize, rand);
+			// compute individual selection probabilities based on fitness
+			computeProbabilities();
+			
+			populationSize = n;
+			population = new int[populationSize];
+			
+			FitnessSelectionOperator.selectProb(N, probabilities, population, populationSize, rand);
 	  }
 
-	    /*
-		* associate probability with individuals
-		*/
+	/*
+	* associate probability with individuals
+	*/
 	  private void computeProbabilities()
-	  {probabilities = new double[N];
-	    double overallProb = 0.0;
-	    for (int j = 0; j < N; j++)
-	      {
-		  // want the smallest fitness value have the greatest probability
-		 probabilities[j] = 1.0 / individuals.get(j).getFitnessValue();	
-		overallProb += probabilities[j];
-	      }
-	  
-	    // make the probabilities with sum equal to 1
-	    for (int j = 0; j < N; j++)
-	      probabilities[j] /= overallProb;
-
-	    for (int j = 1; j < N - 1; j++)
-	      probabilities[j] += probabilities[j - 1];
-	    probabilities[N - 1] = 1.0;
-
-	  }
-
-		/*
-		 * selects individuals for crossover based on probability
-		 */		
-	  private void selectToCrossover()
 	  {
-	    selectProb(CrossoverSize);
+		  	probabilities = new double[N];
+		  	double overallProb = 0.0;
+		  	
+			for (int j = 0; j < N; j++){
+			  // want the smallest fitness value have the greatest probability
+				probabilities[j] = 1.0 / individuals.get(j).getFitnessValue();	
+				overallProb += probabilities[j];
+			}
+	  
+			// make the probabilities with sum equal to 1
+		    for (int j = 0; j < N; j++)
+		      probabilities[j] /= overallProb;
+
+		    for (int j = 1; j < N - 1; j++)
+		      probabilities[j] += probabilities[j - 1];
+	   
+		    probabilities[N - 1] = 1.0;
 	  }
 
-		/*
-		 * selects individuals for mutation based on probability
-		 */		
-	  private void selectToMutate()
-	  {	    
-	      selectProb(MutateSize); 
+	  /*
+	   * Perform the crossovers for this Generation...
+	   */
+	  private void Crossover()
+	  {
+		  CrossoverOperator crosser  = new CrossoverOperator();
+		  
+		  Individual offspring1;
+		  Individual offspring2;
+	  
+		  selectProb(CrossoverSize);
+			
+		  for (int i = 0; i < CrossoverSize - 1; i += 2){			
+		
+			  offspring1 = crosser.CrossOver(individuals.get(crossOverList[i]), individuals.get(crossOverList[i+1]));
+			  offspring2 = crosser.CrossOver(individuals.get(crossOverList[i+1]), individuals.get(crossOverList[i]));
+				
+			  individuals.add(offspring1);
+			  individuals.add(offspring2);
+		  }
 	  }
-
-
-		/*
-		 * select keepers based on probability
-		 */	
-	  private void selectToKeep()
+	  
+	  /*
+	   * Perform all of the Mutation for this Generation...
+	   */
+	  private void Mutate()
+	  {	 
+		  Individual newMutation;	
+		  MutationOperator Mutator = new MutationOperator();
+		  
+		  selectProb(MutateSize); 
+		  
+		  for (int i = 0; i < MutateSize; i++){				
+			newMutation = Mutator.Mutate(individuals.get(population[i]));
+			
+			individuals.add(newMutation);			
+		  }			
+	  }
+	  
+	/*
+	 * select keepers based on probability
+	 */	
+	  private void Keepers()
 	  {	      
-	    //  selectProb(N - #crossover - #mutate);
 	    selectBest(N - CrossoverSize - MutateSize);
+	    
+		for (int i = 0 ; i < populationSize; i++)
+			individuals.add(new Individual());
 	  }
+	  
 
 		/*
 		 * find individual with best fitness
@@ -140,24 +183,21 @@ public class Population {
 	  
 	  private void getBestFitnessValue()
 	  {
-	    BestFitnessValue = individuals.get(0).getFitnessValue();
-	    BestIndividual = 0;
-	  
-	    double diff;
-	    for (int j = 0; j < N; j++)
-	      {
-		diff = individuals.get(j).getFitnessValue() - BestFitnessValue;
-		if (diff < 0)
-		  {
-		    BestFitnessValue = individuals.get(j).getFitnessValue();
-		    BestIndividual = j;
-		  }
-	      }
+		    BestFitnessValue = individuals.get(0).getFitnessValue();
+		    BestIndividual = 0;
+		  
+		    double diff;
+		    
+		    for (int j = 0; j < N; j++){
+		    	
+		    	diff = individuals.get(j).getFitnessValue() - BestFitnessValue;
+		    	if (diff < 0){
+		    		BestFitnessValue = individuals.get(j).getFitnessValue();
+		    		BestIndividual = j;
+		    	}
+		    }
 	  }
-	  
-	  //public void set(int index, Individual individual) {
-		 //individuals[index] = individual;  
-	  //}
+	  	
 	  
 	  protected void NextGeneration(){
 		CrossoverOperator crosser  = new CrossoverOperator();
@@ -175,43 +215,7 @@ public class Population {
 			individuals.add(new Individual());
 		*/
 		
-		int index = 0;
 		
-		selectToKeep();
-		// copy to the next generation
-		for (int i = 0 ; i < populationSize; i++, index++)
-			//nextPopulation[index].set(individuals[population[i]]);
-			nextPopulation.set(index) = this.
-		
+	  }
+	  }
 	  
-		selectToCrossover();
-			
-		Individual offspring1;
-		Individual offspring2;{
-		
-		for (int i = 0; i < Population.populationSize - 1; i += 2){
-			offspring1 = crosser.CrossOver(individuals[population[i]], individuals[population[i+1]]);
-			offspring2 = crosser.CrossOver(individuals[population[i+1]], individuals[population[i]]);
-		    
-			// copy to the next generation
-			for (int i = 0 ; i < populationSize; i++, index++)
-				nextPopulation[index].set(individuals[population[i]]);
-				
-		selectToMutate();
-		Individual newMutation;{
-			for (int i = 0; i < populationSize; i++){				
-				newMutation = Mutator.Mutate(individuals[population[i]]);				
-			}
-			// copy to the next generation
-			for (int i = 0 ; i < populationSize; i++, index++)
-			nextPopulation[index].set(individuals[population[i]]);
-			
-			// replace the old generation
-			for (int i = 0; i < N; i++)
-			  individuals[i].set(nextPopulation[i]);
-		}
-
-	}
-}
-
-}
